635a9df75341d7a7d70640fdceb62471b7cc3292
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-01-09 18:37:10 +0000

    bugfix

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("")
+    $("#gh-repo-data").html("")
+
     var username = $("#gh-username").val(); //gets username from text Filed
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a gitHub usermane.</h2>`) //displays if no user name or no valid username is inputed
@@ -68,4 +71,6 @@ function fetchGitHubInformation(event) {
             }
         });
     }
+
+    $(document),ready(fetchGitHubInformation);
 
